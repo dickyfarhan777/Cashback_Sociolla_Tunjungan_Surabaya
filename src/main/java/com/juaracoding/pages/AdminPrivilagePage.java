d683bbb6ce7a5497e984a491708b767a93c3b533
Dicky Farhan <dikyfarhan7@gmail.com>
2024-01-11 19:35:36 +0700

    Second Commit

---
+++ src/main/java/com/juaracoding/pages/AdminPrivilagePage.java
@@ -0,0 +1,88 @@
+package com.juaracoding.pages;
+
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AdminPrivilagePage {
+    private WebDriver driver;
+
+    public AdminPrivilagePage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//a[contains(text(),'Dashboard')]")
+    private WebElement linkMenuDashboard;
+    @FindBy(xpath = "//h3[normalize-space()='Dashboard Nominal Cashback']")
+    private WebElement txtMenuDashboard1;
+    @FindBy(xpath = "//h3[normalize-space()='Dashboard SPG']")
+    private WebElement txtMenuDashboard2;
+    @FindBy(xpath = "//i[@class='nav-icon fas fa-home']")
+    private WebElement clickMenuHome;
+    @FindBy(xpath = "//i[@class='nav-icon fas fa-registered']")
+    private WebElement clickMenuRegister;
+    @FindBy(xpath = "//i[@class='nav-icon fas fa-tachometer-alt']")
+    private WebElement clickMenuDashboard;
+    @FindBy(xpath = "//i[@class='nav-icon fas fa-users']")
+    private WebElement clickMenuReportData;
+    @FindBy(xpath = "//h1[normalize-space()='List Data Register By User']")
+    private WebElement txtMenuRegister1;
+    @FindBy(xpath = "//h3[@class='card-title']")
+    private WebElement txtMenuReportData1;
+    @FindBy(xpath = "//li[@class='breadcrumb-item active']")
+    private WebElement txtMenuReportData2;
+    @FindBy(xpath = "//h3[@class='card-title']")
+    private WebElement txtMenuRegister2;
+    @FindBy(xpath = "//center[normalize-space()='Selamat datang, admintiara2']")
+    private WebElement txtMenuHome;
+    @FindBy(xpath = "//span[contains(@class, 'login100-form-title p-b-41')]")
+    private WebElement txtLogout;
+    @FindBy(xpath = "//i[@class='nav-icon fas fa-sign-out-alt']")
+    private WebElement clickLogout;
+
+    public void setClickMenuHome(){
+        clickMenuHome.click();
+    }
+    public void setClickMenuDashboard(){
+        clickMenuDashboard.click();
+    }
+    public void setClickMenuRegister(){
+        clickMenuRegister.click();
+    }
+    public void setClickMenuReportData(){
+        clickMenuReportData.click();
+    }
+    public void setClickLogout(){
+        clickLogout.click();
+    }
+
+
+    public String getTxtMenuRegister1(){
+        return txtMenuRegister1.getText();
+    }
+    public String getTxtMenuRegister2(){
+        return txtMenuRegister2.getText();
+    }
+    public String getTxtHome(){
+        return txtMenuHome.getText();
+    }
+    public String getTxtLogout (){
+        return txtLogout.getText();
+    }
+    public String getTxtReportData1(){
+        return txtMenuReportData1.getText();
+    }
+    public String getTxtReportData2(){
+        return txtMenuReportData2.getText();
+    }
+    public String getTxtMenuDashboard1(){
+        return txtMenuDashboard1().getText();
+    }
+    public String getTxtMenuDashboard2(){
+        return txtMenuDashboard2.getText();
+    }
+
+}

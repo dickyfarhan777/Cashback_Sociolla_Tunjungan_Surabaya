7c42be13a92dfeec91ac50b1969e646de7de738a
Dicky Farhan <dikyfarhan7@gmail.com>
2024-01-11 19:50:30 +0700

    Second Commit

--- src/main/java/com/juaracoding/pages/LoginPage.java
+++ src/main/java/com/juaracoding/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.juaracoding.pages;
 
 import com.juaracoding.drivers.DriverSingleton;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -11,5 +12,39 @@ public class LoginPage {
         this.driver = DriverSingleton.getDriver();
         PageFactory.initElements(driver,this);
     }
-    @FindBy()
+    @FindBy(xpath = "//span[contains(@class, 'login100-form-title p-b-41')]")
+    private WebElement txtLogin;
+
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    private WebElement username;
+
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    private WebElement password;
+
+    @FindBy(xpath = "//button[contains(@class, 'login100-form-btn')]")
+    private WebElement btnLogin;
+
+    @FindBy(xpath = "//div[contains(@class, 'panel panel-default')]")
+    private WebElement txtHome;
+
+
+    public void setUsername(String username){
+        this.username.sendKeys(username);
+    }
+    public void setPassword(String password){
+        this.password.sendKeys(password);
+    }
+    public void setBtnLogin(){
+        btnLogin.click();
+    }
+
+
+    public String getTxtLogin (){
+        return txtLogin.getText();
+    }
+    public String getTxtHome(){
+        return txtHome.getText();
+    }
+
+
 }
--- src/test/java/com/juaracoding/LoginTest.java
+++ src/test/java/com/juaracoding/LoginTest.java
@@ -1,8 +1,16 @@
 package com.juaracoding;
 
 import com.juaracoding.pages.LoginPage;
+import com.juaracoding.utils.Constant;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class LoginTest {
     private static WebDriver driver;
@@ -14,7 +22,37 @@ public class LoginTest {
         driver = Hooks.driver;
         extentTest = Hooks.extentTest;
     }
-
-    public void
+    @Given("Admin akses link web cashback")
+    public void admin_enter_link_web_cashback(){
+        driver.get(Constant.URL);
+        extentTest.log(LogStatus.PASS,"Admin akses link web cashback");
+    }
+    @When("Tampilan halaman login")
+    public void tampilan_halaman_login(){
+        loginPage.getTxtLogin();
+        Assert.assertTrue(loginPage.getTxtLogin().contains("SOCIOLLA TUNJUNGAN SURABAYA"));
+        extentTest.log(LogStatus.PASS,"Tampilan halaman login");
+    }
+    @And("Input username admintiara2")
+    public void input_username_admintiara2(){
+        loginPage.setUsername("admintiara2");
+        extentTest.log(LogStatus.PASS,"Input username admintiara2");
+    }
+    @And("Input password a")
+    public void input_password_a(){
+        loginPage.setPassword("a");
+        extentTest.log(LogStatus.PASS,"Input password a");
+    }
+    @And("Klik button login")
+    public void klik_button_login(){
+        loginPage.setBtnLogin();
+        extentTest.log(LogStatus.PASS,"Klik button login");
+    }
+    @Then("Tampilan halaman home")
+    public void tampilan_halaman_home(){
+        loginPage.getTxtHome();
+        Assert.assertEquals(loginPage.getTxtLogin(),"Selamat datang, admintiara2");
+        extentTest.log(LogStatus.PASS,"Tampilan halaman home");
+    }
 
 }

f2b4c54977ff87eaa5d831474911d651d8421e31
Dicky Farhan <dikyfarhan7@gmail.com>
2024-01-11 19:36:40 +0700

    Second Commit

---
+++ src/main/java/com/juaracoding/pages/ReportDataPage.java
@@ -0,0 +1,42 @@
+package com.juaracoding.pages;
+
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ReportDataPage {
+    private WebDriver driver;
+
+    public RegisterPage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+    @FindBy(xpath = "")
+    private WebElement ;
+
+    public void click_menu_report_data(){
+
+    }
+
+}
